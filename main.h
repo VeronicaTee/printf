97f26696a16821fa37fdac93f99eb11dddbcd511
Felix Nzioki Savali <felixnzioki99@gmail.com>
2022-08-08 14:55:31 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,8 +1,18 @@
-#ifndef HEADER
-#define HEADER
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <limits.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
-
+typedef struct buffer_s
+{
+	char *buffer;
+	char *start;
+	unsigned int len;
+} buffer_t;
 
 
 
