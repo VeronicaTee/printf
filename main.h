dd1b4a3a3d1448c1cf3f9252dbbb905093bd2541
Veronica <veronicatomilola@gmail.com>
2022-08-08 02:36:35 -0700

    Write a function that produces output according to a format.

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef HEADER
+#define HEADER
+
+#define BUF_LENGTH 1024
+
+int _printf(const char *format, ...);
+void error_format(const char *format);
+int no_conversion(char);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
