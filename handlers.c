ce2dadc639fa08a3d7b5a95f8285fde97e1a7c96
Veronica <veronicatomilola@gmail.com>
2022-08-09 04:12:13 -0700

    fix betty errors

--- handlers.c
+++ handlers.c
@@ -4,8 +4,6 @@ unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
 
 /**
  * handle_flags - Matches flags with corresponding values.
@@ -160,8 +158,8 @@ int handle_precision(va_list args, const char *modifier, char *index)
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
- *
+ * @int: Integer.
+ * @char: character
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
